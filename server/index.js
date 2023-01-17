468e4242803e65461281eb32c6b7a5a6acce7060
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-01-17 23:37:56 +0530

    spelling

--- server/index.js
+++ server/index.js
@@ -57,13 +57,13 @@ const PORT=process.env.PORT || 6001;
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  application.use(express.static(path.join(__dirname1, "../client/build")));
+  app.use(express.static(path.join(__dirname1, "../client/build")));
 
-  application.get("*", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "../client", "build", "index.html"))
   );
 } else {
-  application.get("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
